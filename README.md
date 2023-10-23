064545d253e8ce5636e4959025d4983b57b89355
Roma Khajiev <raxmon1710@gmail.com>
2023-10-23 14:19:45 +0800

    Added readme file and customized server.py a little bit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # package_checker
-This is a project that I have build for my friend.
+This is a Flask web application that checks the delivery status of packages from a logistics company called 47 Express.
+
+### Installation
+Activate the virtual environment by running the following command:
+```
+source venv/bin/activate
+```
+
+Install the dependencies by running the following command:
+```
+pip install -r requirements.txt
+```
+Run the Flask app by running the following command:
+```
+python server.py
+```
+
+Enter the tracking numbers you want to check in the input field and click the "Check" button.
+
+The app will display the delivery status of each package and the number of days it took to deliver.
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, jsonify, send_file, send_from_directory
 import datetime
+import pandas as pd
+from io import BytesIO
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,13 +9,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
-import pandas as pd
-from io import BytesIO
-from flask_cors import CORS, cross_origin #For deployment
-import os
-
+from flask_cors import CORS, cross_origin  # For deployment
+from flask import Flask, request, jsonify, send_file, send_from_directory
 
 driver = None
+op = webdriver.ChromeOptions()
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("--disable-dev-sh-usage")
+driver = webdriver.Chrome(service=Service(
+    ChromeDriverManager().install()), options=op)
+
+
 def forty_seven_calculate_diff_days(inp):
     # Convert the date strings to datetime objects
     date_format = '%Y/%m/%d %H:%M:%S'
@@ -25,20 +32,26 @@ def forty_seven_calculate_diff_days(inp):
     new_string = f"{inp[2]} {days_difference}"
     # Print the new string
     return new_string
+
+
 def forty_seven_get_date_by_index(index_num):
     try:
         # Wait for the specific <tr> element with data-index="0" and extract the date
         tr_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, f'//tr[@data-index="{index_num}"]'))
+            EC.presence_of_element_located(
+                (By.XPATH, f'//tr[@data-index="{index_num}"]'))
         )
         # Wait for the <div> element within the <td> to be present
         date_element = WebDriverWait(tr_element, 10).until(
-            EC.presence_of_element_located((By.XPATH, './td/div[@class="layui-table-cell laytable-cell-1-0-0"]'))
+            EC.presence_of_element_located(
+                (By.XPATH, './td/div[@class="layui-table-cell laytable-cell-1-0-0"]'))
         )
         date_text = date_element.text
         return date_text
     except Exception as e:
         print("An error occurred:", str(e))
+
+
 def forty_seven_find(tracking_number):
     try:
         # Wait for the textarea element to be present
@@ -57,24 +70,27 @@ def forty_seven_find(tracking_number):
         )
         # Wait for the element to be visible
         ID_status = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, 'layui-table-tool-temp'))
+            EC.visibility_of_element_located(
+                (By.CLASS_NAME, 'layui-table-tool-temp'))
         ).text
         arrived_date = forty_seven_get_date_by_index(0)
-        #GEt the last date
+        # GEt the last date
         try:
             # Use WebDriverWait to wait for elements with 'data-index' to be present
             elements_with_data_index = WebDriverWait(driver, 10).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[data-index]'))
+                EC.presence_of_all_elements_located(
+                    (By.CSS_SELECTOR, '[data-index]'))
             )
 
             # Extract and store the 'data-index' values as integers
-            data_indices = [int(element.get_attribute('data-index')) for element in elements_with_data_index]
+            data_indices = [int(element.get_attribute('data-index'))
+                            for element in elements_with_data_index]
 
             # Find the maximum 'data-index' value
             max_data_index = max(data_indices)
             last_element = forty_seven_get_date_by_index(max_data_index)
             driver.refresh()
-            return last_element,arrived_date, ID_status
+            return last_element, arrived_date, ID_status
 
         except Exception as e:
             print("An error occurred:", str(e))
@@ -86,14 +102,6 @@ def forty_seven_find(tracking_number):
 
 def check_forty_seven(tracking_num):
     # Initialize the Chrome WebDriver
-    # driver = webdriver.Chrome(executable_path=webdriver_path)
-    
-    op = webdriver.ChromeOptions()
-    op.add_argument("--headless")
-    op.add_argument("--no-sandbox")
-    op.add_argument("--disable-dev-sh-usage")
-    global driver
-    driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options=op)
     # Navigate to the webpage
     driver.get('http://47.101.70.255:81/')  # Replace with the actual URL
     # tracking_numbers = [
@@ -111,7 +119,7 @@ def check_forty_seven(tracking_num):
         print(output)
         output = forty_seven_calculate_diff_days(output)
         # Split the output into two columns
-        tracking_num,status, days = output.rsplit(' ')
+        tracking_num, status, days = output.rsplit(' ')
         # Append the data as a tuple
         data.append((f"{tracking_number} {status}", days))
     # Create a DataFrame
@@ -120,14 +128,16 @@ def check_forty_seven(tracking_num):
     # Return a BytesIO object instead of saving the DataFrame to a file
     output_data = BytesIO()
     df.to_excel(output_data, index=False)
-    output_data.seek(0)  # Reset the pointer to the beginning of the BytesIO object
+    # Reset the pointer to the beginning of the BytesIO object
+    output_data.seek(0)
     driver.quit()
     return output_data
-    
 
 
 app = Flask(__name__, static_folder='frontend/build', static_url_path='')
 CORS(app)
+
+
 @app.route('/check', methods=['POST'])
 @cross_origin()
 def check_data():
@@ -145,10 +155,13 @@ def check_data():
             download_name='output.xlsx',
             mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         )
+
+
 @app.route('/', methods=['GET'])
 @cross_origin()
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
